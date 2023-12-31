bc841a74c10ba0fce8ea2daa5273dd5b0f1459de
Rahul Dey <rahuldey832000@gmail.com>
2023-12-31 12:13:30 +0530

    Add: integrating Socket.Io

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import express from "express";
 import { createServer } from "node:http";
-import dotenv from "dotenv";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import { configDotenv } from "dotenv";
 
-// Load environment variables
-dotenv.config();
+configDotenv();
 
-// Create express app and http server
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-// Get server port from environment
 const PORT = process.env.PORT || 3000;
 
-// get path of root directory
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(join(__dirname, "./views/index.html"));
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(` ⚙️ Server listening at http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(PORT, () => {
+  console.log(`⚙️ server running at http://localhost:${PORT}`);
 });
